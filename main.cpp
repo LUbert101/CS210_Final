76788f26554b08fc6c1ef6655fd724486fe4fcf4
LouieUbert <Lubert@mail.bradley.edu>
2025-04-29 09:53:05 -0500

    Implemented main code

--- main.cpp
+++ main.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    const string fileName = "world_cities.csv";
+    Cache cache(10);
+    string countryCode, cityName;
+
+    while (true) {
+        cout << "Enter country code (or 'exit' to quit): ";
+        cin >> countryCode;
+        if (countryCode == "exit") break;
+
+        cout << "Enter city name: ";
+        cin.ignore();
+        getline(cin, cityName);
+
+        string key = countryCode + "," + cityName;
+        string population = cache.get(key);
+
+        if (!population.empty()) {
+            cout << "Population (from cache): " << population << endl;
+        } else {
+            population = searchCSV(fileName, countryCode, cityName);
+            if (population != "Not Found") {
+                cache.put(key, population);
+            }
+            cout << "Population: " << population << endl;
+        }
+
+        cache.display();
+    }
+
     return 0;
 
 }
