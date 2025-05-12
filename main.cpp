0fa454eee4be46fc5dba8e04566bf2def78b78c3
LouieUbert <Lubert@mail.bradley.edu>
2025-05-11 20:42:11 -0500

    Added a search function for trie and reimplemented cashe

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include <fstream>
 #include <unordered_map>
+#include <list>
+#include <random>
+#include <string>
+
 using namespace std;
 
 struct TrieNode {
@@ -25,4 +30,180 @@ class Trie {
         node->isEnd = true;
         node->cityData[cityName] = population;
     }
-}
+
+    int search(const string& cityName, const string& countryCode) {
+        TrieNode* node = root;
+        for (char c : cityName) {
+            if (!node->children[c]) {
+                return -1;
+            }
+            node = node->children[c];
+        }
+        if (node->isEnd && node->cityData.count(countryCode)) {
+            return node->cityData[countryCode];
+        }
+        return -1;
+    }
+};
+
+class Cache {
+private:
+    unordered_map<string, pair<string, list<string>::iterator>> cacheMap;
+    list<string> accessOrder;
+    size_t capacity;
+
+public:
+    Cache(size_t cap) : capacity(cap) {}
+
+    string get(const string& key) {
+        if (cacheMap.find(key) != cacheMap.end()) {
+            // Move accessed key to front of the list
+            accessOrder.erase(cacheMap[key].second);
+            accessOrder.push_front(key);
+            cacheMap[key].second = accessOrder.begin();
+            return cacheMap[key].first;
+        }
+        return "";
+    }
+
+    void put(const string& key, const string& value) {
+        if (cacheMap.find(key) != cacheMap.end()) {
+            // Update value and move key to front
+            accessOrder.erase(cacheMap[key].second);
+        } else if (cacheMap.size() >= capacity) {
+            // Evict least recently used item
+            string lruKey = accessOrder.back();
+            accessOrder.pop_back();
+            cacheMap.erase(lruKey);
+        }
+
+        // Add new item to cache
+        accessOrder.push_front(key);
+        cacheMap[key] = {value, accessOrder.begin()};
+    }
+
+    void display() {
+        cout << "Cache contents:\n";
+        for (const auto& key : accessOrder) {
+            cout << key << " -> " << cacheMap[key].first << "\n";
+        }
+        cout << endl;
+    }
+};
+
+class LFUCache : public Cache {
+    private:
+    unordered_map<string, pair<string, list<string>::iterator>> cacheMap;
+    list<string> accessOrder;
+    size_t capacity;
+    public:
+    LFUCache(size_t cap) : Cache(cap), capacity(cap) {
+    }
+
+    string get(const string& key) {
+        if (cacheMap.find(key) != cacheMap.end()) {
+            accessOrder.erase(cacheMap[key].second);
+            cacheMap[key].second = accessOrder.begin();
+            return cacheMap[key].first;
+        }
+        return "";
+    }
+    void put(const string& key, const string& value) {
+        if (cacheMap.find(key) != cacheMap.end()) {
+            accessOrder.erase(cacheMap[key].second);
+            cacheMap[key].second = accessOrder.begin();
+            cacheMap[key].first = value;
+        } else if (cacheMap.size() >= capacity) {
+            string lruKey = accessOrder.back();
+            accessOrder.pop_back();
+            cacheMap.erase(lruKey);
+        }
+        accessOrder.push_front(key);
+        cacheMap[key] = {value, accessOrder.begin()};
+    }
+    void display() {
+        cout << "Cache contents:\n";
+        for (const auto& key : accessOrder) {
+            cout << key << " -> " << cacheMap[key].first << "\n";
+        }
+        cout << endl;
+    }
+};
+
+class FIFOCache : public Cache {
+    private:
+    unordered_map<string, pair<string, list<string>::iterator>> cacheMap;
+    list<string> accessOrder;
+    size_t capacity;
+    public:
+    FIFOCache(size_t cap) : Cache(cap), capacity(cap) {}
+    string get(const string& key) {
+        if (cacheMap.find(key) != cacheMap.end()) {
+            accessOrder.erase(cacheMap[key].second);
+            cacheMap[key].second = accessOrder.begin();
+            return cacheMap[key].first;
+        }
+        return "";
+    }
+    void put(const string& key, const string& value) {
+        if (cacheMap.find(key) != cacheMap.end()) {
+            accessOrder.erase(cacheMap[key].second);
+            cacheMap[key].second = accessOrder.begin();
+            cacheMap[key].first = value;
+        }
+        accessOrder.push_front(key);
+        cacheMap[key] = {value, accessOrder.begin()};
+    }
+    void display() {
+        cout << "Cache contents:\n";
+        for (const auto& key : accessOrder) {
+            cout << key << " -> " << cacheMap[key].first << "\n";
+        }
+        cout << endl;
+    }
+};
+
+class RandomCache : public Cache {
+    private:
+    unordered_map<string, pair<string, list<string>::iterator>> cacheMap;
+    list<string> accessOrder;
+    size_t capacity;
+    mt19937 rng; // Mersenne Twister random number generator
+    void evictRandomItem() {
+        if (cacheMap.empty()) return;
+        uniform_int_distribution<size_t> dist(0, cacheMap.size() - 1);
+
+        auto it = cacheMap.begin();
+        advance(it, dist(rng));
+
+        accessOrder.erase(it->second.second);
+        cacheMap.erase(it);
+    }
+    public:
+    RandomCache(size_t cap) : Cache(cap), capacity(cap), rng(random_device{}()) {}
+    string get(const string& key) {
+        if (cacheMap.find(key) != cacheMap.end()) {
+            accessOrder.erase(cacheMap[key].second);
+            accessOrder.push_front(key);
+            cacheMap[key].second = accessOrder.begin();
+            return cacheMap[key].first;
+        }
+        return "";
+    }
+    void put(const string& key, const string& value) {
+        if (cacheMap.find(key) != cacheMap.end()) {
+            accessOrder.erase(cacheMap[key].second);
+        } else if (cacheMap.size() >= capacity) {
+            evictRandomItem();
+        }
+        accessOrder.push_front(key);
+        cacheMap[key] = {value, accessOrder.begin()};
+    }
+    void display() {
+        cout << "Cache contents:\n";
+        for (const auto& key : accessOrder) {
+            cout << key << " -> " << cacheMap[key].first << "\n";
+        }
+        cout << endl;
+    }
+};
